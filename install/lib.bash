5a24c6327715c11778cc9a312285c54b82e5f5c7
Artem Chuprina <ran@lasgalen.net>
2017-09-22 19:39:04 +0300

    правки в скрипте установки

--- install/lib.bash
+++ install/lib.bash
@@ -1,3 +1,5 @@
+START_TIME=$(date '+%Y%m%d_%H%M%S')
+
 run () {
 	if [ -n "$DRY_RUN" ]; then
 		echo Would run: "$@"
@@ -23,11 +25,35 @@ throw () {
 	exit 2
 }
 
+INSTALL_STYLE=${INSTALL_STYLE:-working}
 [ -d "$HOME" ] || throw '$HOME not defined'
 [ -n "$USER" ] || throw '$USER not defined'
 [ root != "$USER" ] || throw 'Not suitable for root user'
 TALKREC_DIR=$(pwd)
 SERVICE_DIR="$HOME/.config/systemd/user"
+
+set_install_style () {
+	INSTALL_STYLE=${1:-$INSTALL_STYLE}
+	case "$INSTALL_STYLE" in
+		working) RUN_STYLE=${RUN_STYLE:-systemd} ;;
+		demo) RUN_STYLE=${RUN_STYLE:-screen} ;;
+		*) throw "Unknown install style $INSTALL_STYLE" ;;
+	esac
+	echo "INSTALL_STYLE=$INSTALL_STYLE"
+	echo "RUN_STYLE=$RUN_STYLE"
+}
+
+install_debs () {
+	for deb in "$@"
+	do
+		dpkg -s $deb 2> /dev/null | grep 'Status:.*installed' > /dev/null || INSTALL_DEBS+=($deb)
+	done
+	[ -z "${INSTALL_DEBS[*]}" ] || run sudo apt-get install --yes "${INSTALL_DEBS[@]}"
+}
+
+fix_example_paths_in () {
+	run sed -i -e "s@/home/user/talkrec@$TALKREC_DIR@g" -e "s@/home/user@$HOME@g" "$1"
+}
 
 install_systemd_service () {
 	local FILE="$1"
@@ -35,7 +61,7 @@ install_systemd_service () {
 	run mkdir -p "$SERVICE_DIR"
 	run rm -f "$SERVICE_DIR/$BASE.tmp"
 	run cp "$FILE" "$SERVICE_DIR/$BASE.tmp"
-	run sed -i -e "s!/home/user/talkrec!$TALKREC_DIR!g" -e "s!/home/user!$HOME!g" "$SERVICE_DIR/$BASE.tmp"
+	fix_example_paths_in "$SERVICE_DIR/$BASE.tmp"
 	loginctl show-user "$USER" | grep Linger=yes > /dev/null || run loginctl enable-linger "$USER"
 	if cmp "$SERVICE_DIR/$BASE.tmp" "$SERVICE_DIR/$BASE"; then
 		run rm -f "$SERVICE_DIR/$BASE.tmp"
@@ -43,7 +69,9 @@ install_systemd_service () {
 		run mv "$SERVICE_DIR/$BASE.tmp" "$SERVICE_DIR/$BASE"
 		run systemctl --user daemon-reload
 	fi
-	run systemctl --user enable dispatcher
-	run systemctl --user start dispatcher
-	run systemctl --user status dispatcher
+	run systemctl --user enable "$BASE"
+	run systemctl --user stop "$BASE" || true
+	run systemctl --user start "$BASE"
+	run systemctl --user status "$BASE"
 }
+
