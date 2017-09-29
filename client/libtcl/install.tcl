d5d4063976f89c7d64c8f469c509d016f4033f45
Artem Chuprina <ran@lasgalen.net>
2017-09-29 18:53:10 +0300

    библиотечка для инсталляции

---
+++ client/libtcl/install.tcl
@@ -0,0 +1,17 @@
+proc installDebs {debs} {
+    set need {}
+    foreach deb $debs {
+	set found 0
+	try {
+	    set pipe [open [list | dpkg -s $deb 2>/dev/null]]
+	    while {[gets $pipe line] >= 0} {
+		if {[regexp {^Status:.*installed} $line]} {set found 1}
+	    }
+	    close $pipe
+	}
+	if {!$found} {lappend need $deb}
+    }
+    if {[llength $need] > 0} {
+	run exec sudo apt-get install {*}$need
+    }
+}
