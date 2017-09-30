7041adb93e80cb384d00d4b85ff2ad21897c4563
Artem Chuprina <ran@lasgalen.net>
2017-09-30 15:37:45 +0300

    простенькие демо-запускалки

--- client/libtcl/install.tcl
+++ client/libtcl/install.tcl
@@ -15,3 +15,27 @@ proc installDebs {debs} {
 	run exec sudo apt-get install {*}$need
     }
 }
+
+proc runProcInLoop {args} {
+    set cmd [concat | $args 2>@1]
+    ::log::log notice "Running: $args"
+    if {[catch {open $cmd r} pipe]} {
+	::log::log error "Не удалось запустить $args:\n"
+	return
+    }
+    fconfigure $pipe -blocking 0
+    fileevent $pipe readable [list waitProc $pipe $args]
+}
+
+proc waitProc {pipe cmd} {
+    set data [read $pipe]
+    if {$data ne ""} {puts -nonewline $data; flush stdout}
+    if {[eof $pipe]} {
+	if {[catch {close $pipe} err]} {
+	    ::log::log error "$cmd:\n  $err\nrestart after 12 sec"
+	} else {
+	    ::log::log notice "$cmd finished\nrestart after 12 sec"
+	}
+	after 12000 [concat runProcInLoop $cmd]
+    }
+}
