caa3181912e7924236c922bde54ce0b46d9d5e79
Artem Chuprina <ran@lasgalen.net>
2017-10-05 11:34:03 +0300

    добавлены filesEqual и installSystemdService

--- recorder/libtcl/install.tcl
+++ recorder/libtcl/install.tcl
@@ -39,3 +39,44 @@ proc waitProc {pipe cmd} {
 	after 12000 [concat runProcInLoop $cmd]
     }
 }
+
+proc filesEqual {f1 f2} {
+    if {![file exists $f1] && ![file exists $f2]} {return 1}
+    if {![file exists $f1] || ![file exists $f2]} {return 0}
+    expr {[::md5::md5 -hex -file $f1] eq [::md5::md5 -hex -file $f2]}
+}
+
+proc installSystemdService {exampleFile {service ""}} {
+    set serviceDir [file normalize ~/.config/systemd/user]
+    run file mkdir $serviceDir
+    set content [fixExamplePathsIn [readFile $exampleFile]]
+    if {$service eq ""} {
+	set service [file tail $exampleFile]
+    }
+    if {![regsub {\.service\M.*} $service .service service]} {
+	append service .service
+    }
+    set serviceFile [file join $serviceDir $service]
+    set tmpFile $serviceFile.tmp
+    run file delete -- $tmpFile
+    set fh [run open $tmpFile w]
+    run puts -nonewline $fh $content
+    run close $fh
+    if {[catch {readFile [list | loginctl show-user $::tcl_platform(user)]} showUser]} {
+	set lingerEnabled 0
+    } else {
+	set lingerEnabled [regexp Linger=yes $showUser]
+    }
+    if {!$lingerEnabled} {runExec loginctl enable-linger $::tcl_platform(user)}
+    if {[filesEqual $tmpFile $serviceFile]} {
+	run file delete -- $tmpFile
+    } else {
+	run file rename --force $tmpFile $serviceFile
+	runExec systemctl --user daemon-reload
+    }
+    runExec systemctl --user enable $service
+    catch {runExec systemctl --user stop $service}
+    runExec systemctl --user start $service
+    runExec sleep 2
+    runExec systemctl --user status $service
+}
