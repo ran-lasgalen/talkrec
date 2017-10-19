6c7c23cea9815a6dfe9f25cf87e1c85a24110143
Artem Chuprina <ran@silver.lasgalen.net>
2017-10-19 18:49:18 +0300

    страховка от неуказания юзера

--- db/create_and_grant.sh
+++ db/create_and_grant.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 user="$1"
+if [ -z "$user" ]; then
+	echo "Usage: $0 USER"
+	exit 1
+fi
 psql -c "create database talkrec template template0 encoding 'UTF8' lc_collate 'ru_RU.UTF8' lc_ctype 'ru_RU.UTF8'"
 psql -c "grant all on database talkrec to \"$user\""
 
