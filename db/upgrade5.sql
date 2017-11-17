df5f4605ab7f332159015a5a266b0317e020bc8c
Artem Chuprina <ran@silver.lasgalen.net>
2017-11-17 22:22:54 +0300

    подрихтовал структуру базы данных

---
+++ db/upgrade5.sql
@@ -0,0 +1,8 @@
+alter table phrase_talk alter column phrase_id set not null;
+alter table phrase_talk alter column talk_id set not null;
+alter table site_employee alter column site_id set not null;
+alter table site_employee alter column employee_id set not null;
+alter table phrase_category alter column ord set not null;
+alter table phrase_category alter column title set not null;
+alter table talk add column analyzed boolean not null default 'f';
+alter table phrase add column analyzed boolean not null default 'f';
