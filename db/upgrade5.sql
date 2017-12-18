909af89836c6b00cb9988edff2cb4e9d64ae951d
Artem Chuprina <ran@silver.lasgalen.net>
2017-12-18 22:55:53 +0300

    добавил сдвиг id 1

--- db/upgrade5.sql
+++ db/upgrade5.sql
@@ -4,5 +4,5 @@ alter table site_employee alter column site_id set not null;
 alter table site_employee alter column employee_id set not null;
 alter table phrase_category alter column ord set not null;
 alter table phrase_category alter column title set not null;
-alter table talk add column analyzed boolean not null default 'f';
-alter table phrase add column analyzed boolean not null default 'f';
+alter table talk add column analyzed boolean not null default false;
+alter table phrase add column analyzed boolean not null default false;
---
+++ db/upgrade6.sql
@@ -0,0 +1,1 @@
+update site set id=nextval('site_id_seq') where id=1;
